766907ca703d53fe3e9f572daeb83bafbd452bf1
pwb1133 <qls4328@gmail.com>
2022-10-24 23:39:50 +0900

    test

---
+++ helpers/candyMachineInfo.js
@@ -0,0 +1,17 @@
+export const candyMachineAddress = "0xbd3ebbc735bd76b551c5d5f94642d59cf1e1dff9ba9d17878164fefbbf7fbbc9";
+export const collectionName = "????"; // Case sensitive!
+export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmdPiu2Humo3nXMgyWoKrYQxWjJKhHLPkNEdjjVScbXtPK";
+export const mode = "mainnet"; // "dev" or "test" or "mainnet"
+
+export let NODE_URL;
+let FAUCET_URL;
+if (mode == "dev") {
+    NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
+    FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+} else if (mode === "test") {
+    NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
+    FAUCET_URL = "https://faucet.testnet.aptoslabs.com";
+} else {
+    NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
+    FAUCET_URL = null;
+}
